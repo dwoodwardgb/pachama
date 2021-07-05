50365e7d6eafd7c04a7dadabd34c8ee2398a24e7
David Woodward <davidiwoodward@icloud.com>
2021-07-05 08:38:01 -0400

    Add fuzzy searching

--- client/src/ForestGalleryPage/ForestGalleryPage.js
+++ client/src/ForestGalleryPage/ForestGalleryPage.js
@@ -1,19 +1,24 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import { Link } from "react-router-dom";
+import debounce from "lodash.debounce";
 import ForestCard from "./ForestCard";
 import Layout from "../Layout";
 import "./ForestGalleryPage.css";
 
-function SearchRow() {
+function noop() {}
+
+function SearchRow({ onSearch = noop, onSelect }) {
+  const onSearchDebounced = useCallback(debounce(onSearch, 900), [onSearch]);
+
   return (
     <section className="search-row">
       <section>
         <label>Search</label>
-        <input type="text" name="search" />
+        <input type="text" name="search" onChange={onSearchDebounced} />
       </section>
       <section>
         <label htmlFor="type">Forest type</label>
-        <select id="type">
+        <select id="type" onChange={onSelect}>
           <option value=""></option>
           <option value="conservation">Conservation</option>
           <option value="reforestation">Reforestation</option>
@@ -24,19 +29,30 @@ function SearchRow() {
 }
 
 export default function ForestGalleryPage() {
+  const [query, setQuery] = useState({});
   const [forests, setForests] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:9000/api/forests")
+    fetch("http://localhost:9000/api/forests?" + new URLSearchParams(query))
       .then((r) => r.json())
       .then(setForests);
-  }, [setForests]);
+  }, [setForests, query]);
+
+  const onSearchChange = useCallback(
+    (search) => setQuery({ ...query, name: search.target.value }),
+    [query, setQuery]
+  );
+
+  const onSelectChange = useCallback(
+    (type) => setQuery({ ...query, type: type.target.value }),
+    [query, setQuery]
+  );
 
   return (
     <Layout
       body={
         <>
           <h1>Forests</h1>
-          <SearchRow />
+          <SearchRow onSearch={onSearchChange} onSelect={onSelectChange} />
           <ul className="gallery">
             {forests.map((f) => {
               return (
--- src/api/forest-controller.ts
+++ src/api/forest-controller.ts
@@ -1,10 +1,39 @@
 import { Request, Router } from "express";
+import { object } from "superstruct";
+import { Connection, Repository } from "typeorm";
 import { Forest } from "./forest-entity";
 
 const router = Router();
 
+function onlyTruthyValues(o) {
+  const res = {};
+  Object.entries(o).forEach(([key, val]) => {
+    if (val) {
+      res[key] = val;
+    }
+  });
+  return res;
+}
+
+const fuzzyFields = ["name"];
+
+function buildQuery(db: Connection, query: any) {
+  return Object.entries(query).reduce((builder, [key, val]: any) => {
+    if (fuzzyFields.includes(key)) {
+      // TODO fix injection vulnurability for key
+      return builder.andWhere(`LOWER(${key}) LIKE :${key}`, {
+        [key]: `%${val.toLowerCase()}%`,
+      });
+    } else {
+      // TODO fix injection vulnurability for key
+      return builder.andWhere(`${key} = :${key}`, { [key]: val });
+    }
+  }, db.getRepository(Forest).createQueryBuilder());
+}
+
 router.get("/", async (req: Request, res) => {
-  res.json(await req.db.getRepository(Forest).find());
+  const filteredQuery = onlyTruthyValues(req.query);
+  res.json(await buildQuery(req.db, filteredQuery).getMany());
 });
 
 router.get("/:forestId", async (req: Request, res) => {
